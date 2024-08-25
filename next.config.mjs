4973e0d349d9e486e468295664860b1ddaa4598a
travisseh <travisseh@gmail.com>
2024-08-24 23:40:17 -0600

    trying to fix next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  exportPathMap: async function () {
-    return {
-      "/": { page: "/" },
-      "/about": { page: "/about" },
-      // Add other routes as needed
-    };
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
